c65ffee23e6981f0b54a81e6608a7717caa27bd5
Tyler Holsten <35316637+TylerHolston@users.noreply.github.com>
2020-06-13 17:35:22 -0700

    Finished the call automation

--- voiceGoogle.py
+++ voiceGoogle.py
@@ -3,21 +3,31 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from time import sleep
 import os
 
-TIMEOUT_ERROR = "THERE HAS BEEN A TIMEOUT ERROR. THE XPATH HAS NOT BEEN FOUND"
+TIMEOUT_ERROR_GOOGLE = "THERE HAS BEEN A TIMEOUT ERROR INSIDE GOOGLE SIGNUP. XPATH/NAME WAS NOT FOUND"
+TIMEOUT_ERROR_VOICE = "THERE HAS BEEN A TIMEOUT ERROR INSIDE GOOGLE VOICE. XPATH WAS NOT FOUND"
 TIMEOUT_CONSTANT = 10
 
 class GoogleVoice:
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, phoneNumber):
         self.username = username
         self.password = password
+        self.phoneNumber = phoneNumber
         self.currentDir = os.getcwd()
-        print(self.currentDir)
 
-        self.driver = webdriver.Chrome(self.currentDir + '\chromedriver.exe')
+        #Bypass some browser-related notification/mic access errors
+        option = Options()
+        option.add_experimental_option("prefs", {
+            "profile.default_content_setting_values.media_stream_mic": 1,
+            "profile.default_content_setting_values.media_stream_camera": 2,
+            "profile.default_content_setting_values.geolocation": 1,
+            "profile.default_content_setting_values.notifications": 1
+        })
+        self.driver = webdriver.Chrome(executable_path=self.currentDir + '\chromedriver.exe',options=option)
         self.driver.get("https://www.google.com/")
         self.login()
     
@@ -29,16 +39,30 @@ class GoogleVoice:
             WebDriverWait(self.driver,timeout=TIMEOUT_CONSTANT).until(element_present)
             self.driver.find_element_by_xpath('//*[@id="identifierId"]').send_keys(self.username)
             self.driver.find_element_by_xpath('//*[@id="identifierNext"]').click()
-            element_present = EC.presence_of_all_elements_located((By.XPATH,'//*[@id="password"]/div[1]/div/div[1]/input'))
+            element_present = EC.visibility_of_element_located((By.NAME,"password"))
             WebDriverWait(self.driver,timeout=TIMEOUT_CONSTANT).until(element_present)
-            self.driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys(self.password)
+            self.driver.find_element_by_name("password").send_keys(self.password)
             self.driver.find_element_by_xpath('//*[@id="passwordNext"]').click()
-            sleep(100)
+            element_present = EC.visibility_of_element_located((By.XPATH,'//*[@id="content"]'))
+            WebDriverWait(self.driver,timeout=TIMEOUT_CONSTANT).until(element_present)
         except TimeoutException:
-            print(TIMEOUT_ERROR)
-
-        
-        
+            print(TIMEOUT_ERROR_GOOGLE)
+        finally:
+            self.callManeuver()
+    
+    def callManeuver(self):
+        try:
+            self.driver.get("https://voice.google.com")
+            element_present = EC.presence_of_all_elements_located((By.XPATH,'//*[@id="input_0"]'))
+            WebDriverWait(self.driver,timeout=TIMEOUT_CONSTANT).until(element_present)
+            self.driver.find_element_by_xpath('//*[@id="input_0"]').send_keys(self.phoneNumber)
+            element_present = EC.element_to_be_clickable((By.XPATH,'//*[@id="gvPageRoot"]/div[2]/div[2]/gv-call-sidebar/div/gv-in-call/ng-transclude/gv-make-call-panel/div/div[1]/button'))
+            WebDriverWait(self.driver,timeout=TIMEOUT_CONSTANT).until(element_present)
+            self.driver.find_element_by_xpath('//*[@id="gvPageRoot"]/div[2]/div[2]/gv-call-sidebar/div/gv-in-call/ng-transclude/gv-make-call-panel/div/div[1]/button').click()
+        except TimeoutException:
+            print(TIMEOUT_ERROR_VOICE)
+        finally:
+            sleep(10)
 
 if __name__ == "__main__":
-    GoogleVoice("username","password")
+    GoogleVoice("soccerback1","Emmadog99$$$","+17602674456")
